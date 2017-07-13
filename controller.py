198fb4968c40f2344dd822290fb1baac97d24ff4
Dylan Rush <dylanhrush@gmail.com>
2017-07-13 23:28:40 +0000

    It works

--- controller.py
+++ controller.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 import urllib
-import urllib,re
+import re
+from threading import Thread
+
 def get_metar(airport):
   try:
     stream = urllib.urlopen('http://www.aviationweather.gov/metar/data?ids='+airport+'&format=raw&hours=0&taf=off&layout=off&date=0');
@@ -72,6 +74,44 @@ colors = {'RED':(GPIO.HIGH, GPIO.LOW, GPIO.LOW),
 'BLUE':(GPIO.LOW, GPIO.LOW, GPIO.HIGH),
 'LOW':(GPIO.LOW, GPIO.LOW, GPIO.LOW)}
 
+airport_flasher = True
+airport_should_flash = {}
+airport_color = {}
+
+for airport in airport_pins:
+  airport_should_flash[airport] = True
+  airport_color[airport] = 'BLUE'
+
+def set_airport_display(airport, category):
+  if category == 'VFR':
+    airport_should_flash[airport] = False
+    airport_color[airport] = 'GREEN'
+  elif category == 'MVFR':
+    airport_should_flash[airport] = False
+    airport_color[airport] = 'BLUE'
+  elif category == 'IFR':
+    airport_should_flash[airport] = False
+    airport_color[airport] = 'RED'
+  elif category == 'LIFR':
+    airport_should_flash[airport] = True
+    airport_color[airport] = 'RED'
+  else:
+    airport_should_flash[airport] = True
+    airport_color[airport] = 'BLUE'
+
+def refresh_airport_displays():
+  for airport in airport_pins:
+    category = get_category(get_metar(airport))
+    set_airport_display(airport, category)
+
+def render_airport_displays():
+  for airport in airport_pins:
+    if airport_should_flash[airport] and airport_flasher == True:
+      setLed(airport_pins[airport], 'LOW')
+    else:
+      setLed(airport_pins[airport], airport_color[airport])
+  airport_flasher = bool(not airport_flasher)
+
 #VFR - Green
 #MVFR - Blue
 #IFR - Red
@@ -88,21 +128,39 @@ def all_airports(color):
     GPIO.setup(airport_pins[airport], GPIO.OUT)
     setLed(airport_pins[airport], color)
 
+def render_thread():
+  while(True):
+    render_airport_displays
+    time.sleep(1)
+
+def refresh_thread():
+  while(True):
+    refresh_airport_displays
+    time.sleep(60)
+
 
 
 GPIO.setmode(GPIO.BOARD)
 # Test LEDS on startup
-all_airports('PURPLE')
+all_airports('GREEN')
 
-time.sleep(5)
+time.sleep(2)
 
 all_airports('LOW')
 
-time.sleep(5)
+time.sleep(2)
 
-all_airports('PURPLE')
+all_airports('GREEN')
 
-time.sleep(5)
+time.sleep(2)
+
+thread1 = Thread(target = render_thread)
+thread2 = Thread(target = refresh_thread)
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
 
 GPIO.cleanup()
 
