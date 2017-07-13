27958d714dec2b5254b7860896899aabd87218c3
Dylan Rush <dylanhrush@gmail.com>
2017-07-13 23:02:08 +0000

    second commit

--- controller.py
+++ controller.py
@@ -1,6 +1,63 @@
 import RPi.GPIO as GPIO
 import time
 import urllib
+import urllib,re
+def get_metar(airport):
+  try:
+    stream = urllib.urlopen('http://www.aviationweather.gov/metar/data?ids='+airport+'&format=raw&hours=0&taf=off&layout=off&date=0');
+    for line in stream:
+      if '<!-- Data starts here -->' in line:
+        return re.sub('<[^<]+?>', '', stream.readline())
+    return 'INVALID'
+  except Exception, e:
+    print str(e);
+    return 'INVALID'
+  finally:
+    stream.close();
+def get_vis(metar):
+  components = metar.split(' ');
+  for component in components:
+    if 'SM' in component:
+      return re.sub('SM', '', component)
+  return 'INVALID'
+def get_vis_category(vis):
+  if '/' in vis:
+    return 'LIFR'
+  vis_int = int(vis)
+  if vis < 3:
+    return 'IFR'
+  if vis <= 5:
+    return 'MVFR'
+  return 'VFR'
+def get_ceiling(metar):
+  components = metar.split(' ' );
+  minimum_ceiling = 10000
+  for component in components:
+    if 'BKN' in component or 'OVC' in component:
+      ceiling = int(filter(str.isdigit,component)) * 100
+      if(ceiling < minimum_ceiling):
+        minimum_ceiling = ceiling
+  return minimum_ceiling
+def get_ceiling_category(ceiling):
+  if ceiling < 500:
+    return 'LIFR'
+  if ceiling < 1000:
+    return 'IFR'
+  if ceiling < 3000:
+    return 'MVFR'
+  return 'VFR'
+def get_category(metar):
+  vis = get_vis_category(get_vis(metar))
+  ceiling = get_ceiling_category(get_ceiling(metar))
+  if(vis == 'INVALID' or ceiling == 'INVALID'):
+    return 'INVALID'
+  if(vis == 'LIFR' or ceiling == 'LIFR'):
+    return 'LIFR'
+  if(vis == 'IFR' or ceiling == 'IFR'):
+    return 'IFR'
+  if(vis == 'MVFR' or ceiling == 'MVFR'):
+    return 'MVFR'
+  return 'VFR'
 
 airport_pins = {'KRNT':(3,5,7),
         'KSEA':(11,13,15),
@@ -12,7 +69,16 @@ airport_pins = {'KRNT':(3,5,7),
 
 colors = {'RED':(GPIO.HIGH, GPIO.LOW, GPIO.LOW),
 'GREEN':(GPIO.LOW, GPIO.HIGH, GPIO.LOW),
-'BLUE':(GPIO.LOW, GPIO.LOW, GPIO.HIGH)}
+'BLUE':(GPIO.LOW, GPIO.LOW, GPIO.HIGH),
+'LOW':(GPIO.LOW, GPIO.LOW, GPIO.LOW)}
+
+#VFR - Green
+#MVFR - Blue
+#IFR - Red
+#LIFR - Flashing red
+#Error - Flashing blue
+
+
 
 def setLed(pins, color):
   GPIO.output(pins, colors[color])
@@ -25,16 +91,16 @@ def all_airports(color):
 
 
 GPIO.setmode(GPIO.BOARD)
-
-all_airports('RED')
-
-time.sleep(5)
-
-all_airports('GREEN')
+# Test LEDS on startup
+all_airports('PURPLE')
 
 time.sleep(5)
 
-all_airports('BLUE')
+all_airports('LOW')
+
+time.sleep(5)
+
+all_airports('PURPLE')
 
 time.sleep(5)
 
