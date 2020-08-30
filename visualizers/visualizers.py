f211fe496c85985ea83d8e8de35b4ffff9204947
john.marzulli@hotmail.com <jmarzulli@pop-os.localdomain>
2020-08-30 02:28:41 -0700

    Add precipitation visualizer into official list.

--- visualizers/visualizers.py
+++ visualizers/visualizers.py
@@ -4,6 +4,7 @@ from visualizers import flight_rules, rainbow, weather
 VISUALIZERS = [
     flight_rules.FlightRulesVisualizer(LOGGER),
     weather.TemperatureVisualizer(LOGGER),
+    weather.PrecipitationVisualizer(LOGGER),
     rainbow.RainbowVisualizer(LOGGER),
     rainbow.LightCycleVisualizer(LOGGER)
 ]
